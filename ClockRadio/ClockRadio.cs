387e401e7c2bbdcedb23f43d7e9ef7c072e03a33
alynielson <nielsonaly@gmail.com>
2018-07-23 09:45:25 -0500

    create member variables

---
+++ ClockRadio/ClockRadio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    public class ClockRadio
+    {
+        //member variables
+        public DateTime currentDateTime;
+        public string currentState;
+        public string currentRadioStation;
+
+        //constructor
+
+
+        //member methods
+    }
+}
